617e420c0dbf35f4564678325736a38a28d6a995
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-07-31 08:13:06 -0300

    feat: adiciona nova props que carrega valores css
    cria uma props com o nome de className para levar um objeto com valores css

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,12 +1,13 @@
 import styles from "./Card.module.css"
 
 interface CardProps {
-    children: React.ReactNode
+    children: React.ReactNode,
+    className?: object | {} 
 }
 
-export const Card = ({children}: CardProps) => {
+export const Card = ({ children, className }: CardProps) => {
     return (
-        <div className={styles.card}>
+        <div className={styles.card} style={className}>
             {children}
         </div>
     )
