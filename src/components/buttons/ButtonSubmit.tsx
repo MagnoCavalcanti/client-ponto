5a96448a776e0d84e647170349935791ceba9766
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-08-30 17:19:58 -0300

    feat(UI): adiciona ao componente ButtonSubmit um suporte a um icon da lib lucide

--- src/components/buttons/ButtonSubmit.tsx
+++ src/components/buttons/ButtonSubmit.tsx
@@ -1,13 +1,18 @@
+import React from "react";
 import Style from "./ButtonSubmit.module.css"
 
 interface ButtonSubmitProps {
     children: string;
+    icon?: React.ElementType;
 }
 
-export const ButtonSubmit = ({ children }: ButtonSubmitProps) => {
+export const ButtonSubmit = ({ children, icon }: ButtonSubmitProps) => {
     
 
     return (
-        <button type="submit" className={Style.buttonSubmit} >{children}</button>
+        <button type="submit" className={Style.buttonSubmit} >
+            {icon && <span className={Style.icon}>{React.createElement(icon)}</span>}
+            {children}
+        </button>
     )
 }
