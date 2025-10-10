4c6c74f3f838a0a052e2e9ba8f34bfed7e4d91a0
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-10-10 04:13:26 -0300

    feat(API): adiciona a requisição de login

---
+++ src/lib/apiLogin.tsx
@@ -0,0 +1,36 @@
+import type { User } from "../types/User";
+import { apiLogin } from "./axios";
+
+// src/services/authService.ts
+
+
+
+
+interface LoginResponse {
+  access_token: string;// se houver
+  token_type: string;
+  exp: string 
+}
+
+export async function login({ username, password }: User): Promise<boolean> {
+  try {
+    console.log(username + " - " + password);
+    
+    const response = await apiLogin.post<LoginResponse>("/login",
+        { username, password }, // envia JSON
+        {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }, // garante que é JSON
+        }    
+    );
+    const { access_token } = response.data;
+    
+
+    // salva token para requisições futuras
+    localStorage.setItem("sp:accessToken", access_token);
+
+    return true;
+  } catch (error: any) {
+    console.error("Erro ao fazer login:", error.response?.data || error.message);
+    throw error;
+  }
+}
